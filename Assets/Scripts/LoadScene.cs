a2fb6c38298d6196f969fc363f272dca1c1c01ba
CackaUY <95343855+CackaUY@users.noreply.github.com>
2022-11-14 23:58:42 +0300

    load scene
    start

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,12 @@
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{ 
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene(1);
+    }  
+}
